a82e9e01f83c69192e9e02de12b8d8541a102db9
Lin Yun Wen <l40303k@gmail.com>
2021-04-23 14:51:24 +0800

    Feature: setup env

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: path.resolve('./src/index.js')
+    },
+    output: {
+        path: path.resolve('./build'),
+        filename: '[name].js',
+        publicPath: path.resolve('./build/public')
+    },
+    target: 'web',
+    devtool: 'inline-source-map',
+    devServer: {
+        inline: true,
+        port: 7777
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    resolveLoader: {
+        modules: ['node_modules', 'build-tools/js-loaders'],
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html?$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+              },
+            {
+                test: /\.s?css$/,
+                include: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            fallback: 'file-loader',
+                            name: '[path][name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        // concatenateModules: false,
+        minimizer: [
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: "/*!! Dcard reader © Lin Yun Wen */",
+            raw: true
+        })
+    ]
+};
+
