7d2eb56f3bcfd5862273206f102e884fbd77fd7c
Lin Yun Wen <l40303k@gmail.com>
2021-04-29 21:01:09 +0800

    Feature: modify the style of the card

--- src/PostCard.js
+++ src/PostCard.js
@@ -12,10 +12,12 @@ export class PostCard extends React.Component {
     console.log(this.post, this.post.mediaMeta, this.post.mediaMeta[0])
     return (
     <div className="post">
-        <Paper style={{ margin: "5%", padding: "10px" }}>
+        <Paper style={{ marginBottom: "10px", padding: "20px" }}>
             <Grid container>
                 <Grid item sm={3}>
-                    <img style={{ maxHeight: "128px", maxWidth: "128px" }} src={this.post.mediaMeta[0]?.thumbnail}></img>
+                    <div style={{ height: "130px", overflow: "hidden"}}>
+                        <img style={{ maxWidth: "150px" }} src={this.post.mediaMeta[0]?.thumbnail}></img>
+                    </div>
                 </Grid>
                 <Grid item sm={9} container>
                     <Grid item>
