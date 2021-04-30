846c3112227a3fecbb29423601d14270853c10b1
Lin Yun Wen <l40303k@gmail.com>
2021-04-30 08:31:19 +0800

    Feature: add the count of reactions and comments

--- src/PostCard.js
+++ src/PostCard.js
@@ -9,10 +9,21 @@ export class PostCard extends React.Component {
   }
 
   genReactions(reactions) {
+        let emojis = {
+            "286f599c-f86a-4932-82f0-f5a06f1eca03": "❤️",
+            "011ead16-9b83-4729-9fde-c588920c6c2d": "🤩",
+            "4b018f48-e184-445f-adf1-fc8e04ba09b9": "😮",
+            "aa0d425f-d530-4478-9a77-fe3aedc79eea": "😡",
+            "e8e6bc5d-41b0-4129-b134-97507523d7ff": "😂",
+            "514c2569-fd53-4d9d-a415-bf0f88e7329f": "😭"
+        };
       return reactions.map((reaction, index) => {
-          return (<Grid item key={index}>
-            <span>{reaction.count}</span>
-        </Grid>)
+            return (<Grid item key={index}>
+                <Grid container spacing={1}>
+                    <Grid item>{emojis[reaction.id]}</Grid>
+                    <Grid item>{reaction.count}</Grid>
+                </Grid>
+            </Grid>);
       });
   }
 
@@ -52,6 +63,12 @@ export class PostCard extends React.Component {
                     </Grid>
                     <Grid item>
                         <Grid item container spacing={2}>
+                            <Grid item>
+                                <Grid container spacing={1}>
+                                    <Grid item>💬</Grid>
+                                    <Grid item>{this.post.commentCount}</Grid>
+                                </Grid>
+                            </Grid>
                             {this.genReactions(this.post.reactions)}
                         </Grid>
                     </Grid>
