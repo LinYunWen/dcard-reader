72b5620b2efe0657782abdf8519512e63c0bdc3d
Lin Yun Wen <l40303k@gmail.com>
2021-05-01 14:05:25 +0800

    Feature: add sekleton

--- src/PostCard.js
+++ src/PostCard.js
@@ -1,6 +1,5 @@
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
